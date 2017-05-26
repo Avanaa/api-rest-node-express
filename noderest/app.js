d61b947ff402560b8b034a2dfefd452ab776d3eb
Avana Araujo <Avana Araujo>
2017-05-26 12:10:27 -0300

    Add router test

--- noderest/app.js
+++ noderest/app.js
@@ -3,5 +3,5 @@ var app = require('./config/express-config')();
 var port = process.env.port || 3001;
 
 app.listen(port, function(){
-    console.log('Servidor rodando na porta 3000..');
+    console.log('Servidor rodando na porta 3001..');
 });
